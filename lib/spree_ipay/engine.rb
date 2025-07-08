df6f53a9ad21abef2dc120e87d0e3bbbb9d1c507
Symomkuu <simonnjunge350@gmail.com>
2025-07-08 13:35:18 +0300

    Optimize engine configuration and fix view loading

--- lib/spree_ipay/engine.rb
+++ lib/spree_ipay/engine.rb
@@ -1,27 +1,47 @@
 module SpreeIpay
   class Engine < ::Rails::Engine
     engine_name 'spree_ipay'
+    isolate_namespace Spree
 
-    config.autoload_paths += %W[#{config.root}/lib]
-    config.autoload_paths += %W[#{config.root}/app/models]
+    # Configure autoload paths
+    config.autoload_paths += %W[
+      #{config.root}/lib
+      #{config.root}/app/models
+    ]
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
+    # Configure generators
+    config.generators do |g|
+      g.test_framework :rspec
     end
 
-    config.to_prepare(&method(:activate).to_proc)
+    # Add views to load paths
+    initializer 'spree_ipay.add_views', after: :load_config_initializers do |app|
+      # Add gem's views to the main app's view path
+      app.config.paths['app/views'].unshift(config.root.join('app/views'))
+      
+      # Add spree views path
+      config.paths['app/views'] << 'app/views/spree'
+    end
 
+    # Configure assets precompilation
     initializer 'spree_ipay.assets' do |app|
       app.config.assets.precompile += %w[spree/backend/ipay.js]
     end
 
+    # Register the payment method
     initializer 'spree_ipay.register_payment_method', after: 'spree.register.payment_methods' do |app|
       Rails.application.config.after_initialize do
-        if defined?(Spree::PaymentMethod) && defined?(Spree::PaymentMethod::Ipay) && !app.config.spree.payment_methods.include?(Spree::PaymentMethod::Ipay)
+        if defined?(Spree::PaymentMethod) && defined?(Spree::PaymentMethod::Ipay) && 
+           !app.config.spree.payment_methods.include?(Spree::PaymentMethod::Ipay)
           app.config.spree.payment_methods << Spree::PaymentMethod::Ipay
         end
+      end
+    end
+    
+    # Load decorators
+    config.to_prepare do
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require_dependency(c) : load(c)
       end
     end
   end
