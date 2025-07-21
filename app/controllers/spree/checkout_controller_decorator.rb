6277272bbcb093fb06e2af102bbf6efb26b46b5c
Symomkuu <simonnjunge350@gmail.com>
2025-07-21 18:46:17 +0300

    new changes

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -92,9 +92,11 @@ module Spree
       vid = (ipay_method.preferred_vendor_id.presence || '').downcase[0...12] # Max 12 chars, lowercase
       curr = (ipay_method.preferred_currency.presence || 'KES')[0...3] # Max 3 chars
       
-      # Prepare callback URL - remove any invalid characters
-      cbk = (ipay_method.preferred_callback_url.presence || "https://#{ipay_method.base_url}/ipay/confirm").gsub(/[;~`!%^*\-\u003e\u003c\u0026_]/i, '')
-      
+      # Prepare callback URL from preferences - remove any invalid characters
+      cbk = (ipay_method.preferred_callback_url.presence).to_s.gsub(/[;~`!%^*\-\u003e\u003c\u0026_]/i, '')
+      # Prepare return URL from preferences - remove any invalid characters
+      lbk = (ipay_method.preferred_return_url.presence).to_s.gsub(/[;~`!%^*\-\u003e\u003c\u0026_]/i, '')
+
       # Prepare iPay parameters - must match the exact order and parameters used in hash generation
       ipay_params = {
         'live' => live,
