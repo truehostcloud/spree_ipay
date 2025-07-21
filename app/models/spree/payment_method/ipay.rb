fe5991586466789d2a47851b15ed59ba7a8f35bd
Symomkuu <simonnjunge350@gmail.com>
2025-07-21 18:14:33 +0300

    new changes

--- app/models/spree/payment_method/ipay.rb
+++ app/models/spree/payment_method/ipay.rb
@@ -16,7 +16,7 @@ module Spree
     # Core settings (in display order)
     preference :vendor_id, :string
     preference :hash_key, :string
-    preference :test_mode, :boolean, default: true
+    preference :test_mode, :boolean, default: false # Live mode is default
     preference :currency, :string, default: 'KES'
     preference :callback_url, :string, default: '/ipay/confirm'
     preference :return_url, :string, default: -> {
@@ -25,14 +25,14 @@ module Spree
 
     # Payment channels (in display order)
     preference :mpesa, :boolean, default: true
-    preference :airtel, :boolean, default: false
-    preference :equity, :boolean, default: false
+    preference :airtel, :boolean, default: true
+    preference :equity, :boolean, default: true
+    preference :creditcard, :boolean, default: true
+    preference :pesalink, :boolean, default: true
     preference :mobilebanking, :boolean, default: false
-    preference :creditcard, :boolean, default: false
     preference :unionpay, :boolean, default: false
     preference :mvisa, :boolean, default: false
     preference :vooma, :boolean, default: false
-    preference :pesalink, :boolean, default: false
     preference :autopay, :boolean, default: false
 
     # Ensure preferences are sorted in the desired display order
@@ -710,7 +710,7 @@ module Spree
     end
 
     def api_endpoint
-      preferred_test_mode ? 'https://sandbox.ipayafrica.com/v3/ke' : 'https://payments.ipayafrica.com/v3/ke'
+      'https://payments.ipayafrica.com/v3/ke' # Always use live endpoint
     end
 
     def success_response(message = 'Success')
