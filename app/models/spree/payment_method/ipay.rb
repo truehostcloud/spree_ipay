ee58781369d0cbe48e1fd3182422f38f40035e4f
Symomkuu <simonnjunge350@gmail.com>
2025-07-22 20:35:07 +0300

    Fix: Round up amount consistently in ipay.rb

--- app/models/spree/payment_method/ipay.rb
+++ app/models/spree/payment_method/ipay.rb
@@ -306,8 +306,9 @@ module Spree
       # Prepare values - must match exactly what will be sent in the form
       oid = payment.order.number.to_s
       inv = "#{payment.order.number}#{Time.now.to_i}" # unique invoice
-      # Round up to nearest whole number for iPay
-      ttl = payment.amount.ceil.to_i.to_s
+      # Calculate amount once to ensure consistency
+      amount = payment.amount.ceil.to_i
+      ttl = amount.to_s
       tel = phone.presence || payment.order.bill_address&.phone.to_s.presence || "0700000000"
       eml = payment.order.email.to_s
       vid = vendor_id
@@ -340,8 +341,9 @@ module Spree
       oid = payment.order.id.to_s
       # Use numeric order ID for invoice as well
       inv = payment.order.id.to_s
-      # Round up to nearest whole number for iPay
-      ttl = payment.amount.ceil.to_i.to_s
+      # Use the same amount calculation as in signature generation
+      amount = payment.amount.ceil.to_i
+      ttl = amount.to_s
       tel = payment.order.bill_address&.phone || session[:ipay_phone_number] || "0700000000"
       eml = payment.order.email
       vid = preferred_vendor_id
